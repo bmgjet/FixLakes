633c5e9698ebe7859f8dd6e15bf327a0cb66008a
bmgjet <50484759+bmgjet@users.noreply.github.com>
2024-07-05 00:45:39 +1200

    Upload

---
+++ FixLakes/FixlakesGUI.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using ProtoBuf;
+
+namespace FixLakes
+{
+    public partial class FixlakesGUI : Form
+    {
+        public FixlakesGUI()
+        {
+            InitializeComponent();
+        }
+
+        private void Fixbutton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = @"C:\",
+                Title = "Browse Map Files",
+                CheckFileExists = true,
+                CheckPathExists = true,
+                DefaultExt = "map",
+                Filter = "map files (*.map)|*.map",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                WorldSerialization worldData = new WorldSerialization();
+                worldData.Load(openFileDialog1.FileName);
+                int found = 0;
+                foreach (PrefabData prefabData in worldData.world.prefabs)
+                {
+                    if(prefabData.id == 2666567578)
+                    {
+                        found++;
+                        prefabData.id = 3698016822;
+                    }
+                }
+                MessageBox.Show("Fixed " + found + " Lake IDs", "Fixed");
+                worldData.Save(openFileDialog1.FileName.Replace(".map", "-Fixed.map"));
+            }
+        }
+    }
+}
---
+++ FixLakes/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FixLakes
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += (sender, bargs) =>
+            {
+                String dllName = new AssemblyName(bargs.Name).Name + ".dll";
+                var assem = Assembly.GetExecutingAssembly();
+                String resourceName = assem.GetManifestResourceNames().FirstOrDefault(rn => rn.EndsWith(dllName));
+                if (resourceName == null) return null; // Not found, maybe another handler will find it
+                using (var stream = assem.GetManifestResourceStream(resourceName))
+                {
+                    Byte[] assemblyData = new Byte[stream.Length];
+                    stream.Read(assemblyData, 0, assemblyData.Length);
+                    return Assembly.Load(assemblyData);
+                }
+            };
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FixlakesGUI());
+        }
+    }
+}
